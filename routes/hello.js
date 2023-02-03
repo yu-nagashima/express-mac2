9555f89ddc542fd321b86476fc6786776396d027
長島雄平 <yu-nagashima@nagashimayuuheinoMacBook-Pro.local>
2023-02-03 14:43:03 +0900

    add hello js part2

--- routes/hello.js
+++ routes/hello.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello, { title: 'Express' });
+  res.render('hello', { title: 'hello Express' });
 });
 
 module.exports = router;
